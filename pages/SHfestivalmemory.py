109c9c056431d52b9d5534d18c1a90bebc0cd0e3
sanghak31 <h2510723@joongdong.hs.kr>
2025-12-20 00:39:35 +0900

    Update SHfestivalmemory.py

--- pages/SHfestivalmemory.py
+++ pages/SHfestivalmemory.py
@@ -35,7 +35,7 @@ def get_level_config(level):
         8: {'grid_rows': 5, 'grid_cols': 5, 'pairs': 11, 'max_failures': 14, 'bombs': 3,
             'has_ice': False, 'has_light': False, 'has_witch': False, 'has_lock': True, 'has_ball': True, 'has_joker': True},
         9: {'grid_rows': 6, 'grid_cols': 6, 'pairs': 7, 'max_failures': 8, 'bombs': 22,
-            'has_ice': True, 'has_light': False, 'has_witch': False, 'has_lock': True, 'has_ball': True, 'has_joker': False},
+            'has_ice': False, 'has_light': False, 'has_witch': False, 'has_lock': True, 'has_ball': True, 'has_joker': True},
         10: {'grid_rows': 6, 'grid_cols': 6, 'pairs': 15, 'max_failures': 15, 'bombs': 6,
              'has_ice': True, 'has_light': True, 'has_witch': True, 'has_lock': True, 'has_ball': True, 'has_joker': True},
     }
