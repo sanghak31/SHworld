15d3b0f9f5f66998e7f7271d0c3133b6e9a4859f
sanghak31 <h2510723@joongdong.hs.kr>
2026-07-02 13:54:12 +0900

    Update biogenetic.py

--- biogenetic.py
+++ biogenetic.py
@@ -346,6 +346,11 @@ if st.session_state.converted:
     st.markdown(color_sequence(st.session_state.rna),
                 unsafe_allow_html=True)
 
+    st.metric(
+        "DNA 길이",
+        f"{st.session_state.total} bp"
+    )
+
     st.write("### 염기 개수 통계")
 
     col1, col2, col3, col4 = st.columns(4)
